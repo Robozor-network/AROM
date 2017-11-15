836e4cd3a3dbf16c0947fb69604bb8738707f6c8
Roman Dvořák <roman-dvorak@email.cz>
2017-11-15 19:08:32 +0100

    helper

--- module/src/arom_helper.py
+++ module/src/arom_helper.py
@@ -3,18 +3,23 @@
 
 import rospy
 import os
+import arom
+from arom import srv 
 
 class AromNode():
     def __init__(self):
 
         try:
+            print "AromNode INIT"
             if self.node_pymlab:
-                #self.pymlabService = rospy.ServiceProxy('pymlab_drive', PymlabDrive)
-                self.pymlab = rospy.ServiceProxy('pymlab_drive', PymlabDrive)
+                self.pymlab = rospy.ServiceProxy('pymlab_drive', srv.PymlabDrive)
                 rospy.set_param('/arom/node'+rospy.get_name()+"/pymlab", True)
             else:
                 rospy.set_param('/arom/node'+rospy.get_name()+"/pymlab", False)
+                print "Chyba pri vytvareni pymlabu"
+                rospy.logerr("Chyba pri vytvareni pymlabu")
         except Exception, e:
+            print e
             rospy.set_param('/arom/node'+rospy.get_name()+"/pymlab", False)
 
         print "Starting init"
@@ -24,7 +29,7 @@ class AromNode():
         print "Init done:", rospy.get_name()
 
     #def pymlab(self, *args, **kwds):
-    #    self.pymlabService(**kwds)
+        #self.pymlabService(**kwds)
 
     def set_feature(self, name, value):
         rospy.set_param('/arom/node%s/feature/%s' %(str(rospy.get_name()),name), value)
