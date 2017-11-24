9fd02283a10c7fe2de2a12824934ea280fa50e09
roman-dvorak <romandvorak@mlab.cz>
2017-11-24 13:30:26 +0100

    pymlab br

--- src/drivers/arom_tools.py
+++ src/drivers/arom_tools.py
@@ -9,8 +9,15 @@ from arom.msg import *
 class AromNode():
     def __init__(self):
 
-        if self.node_pymlab:
-            self.pymlabService = rospy.ServiceProxy('pymlab_drive', PymlabDrive)
+        try:
+            if self.node_pymlab:
+                #self.pymlabService = rospy.ServiceProxy('pymlab_drive', PymlabDrive)
+                self.pymlab = rospy.ServiceProxy('pymlab_drive', PymlabDrive)
+                rospy.set_param('/arom/node'+rospy.get_name()+"/pymlab", True)
+            else:
+                rospy.set_param('/arom/node'+rospy.get_name()+"/pymlab", False)
+        except Exception, e:
+            rospy.set_param('/arom/node'+rospy.get_name()+"/pymlab", False)
 
         print "Starting init"
         rospy.init_node(self.node_name)
@@ -18,12 +25,9 @@ class AromNode():
         self.node_name = rospy.get_name()
         print "Init done:", rospy.get_name()
 
-    def pymlab(self, *args, **kwds):
-        self.pymlabService(**kwds)
+    #def pymlab(self, *args, **kwds):
+    #    self.pymlabService(**kwds)
 
-class Pymlab():
-    def __init__(self):
-        self.serv = rospy.ServiceProxy('pymlab_drive', PymlabDrive)
-
-    def send(self, device=None, method=None, parameters=None):
-        return self.serv(device=device, method=method, parameters=parameters)
+    def set_feature(self, name, value):
+        rospy.set_param('/arom/node%s/feature/%s' %(str(rospy.get_name()),name), value)
+        rospy.loginfo("New feature '%s' for %s" %(rospy.get_name(), name))
